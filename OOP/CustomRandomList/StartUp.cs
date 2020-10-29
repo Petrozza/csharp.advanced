62a8fc9298b7df4e0571635bf7f9554f6fa83077
Angel Petrov <64444672+Petrozza@users.noreply.github.com>
2020-10-29 03:17:39 +0200

    add new code

---
+++ OOP/CustomRandomList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("acho");
+            randomList.Add("aort");
+            randomList.Add("nuru");
+            randomList.Add("nti");
+
+            Console.WriteLine(randomList.Count);
+            Console.WriteLine(randomList.RandomString());
+            Console.WriteLine(randomList.Count);
+
+        }
+    }
+}
